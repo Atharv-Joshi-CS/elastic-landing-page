a8a22c2fbae06fbbaa6d316afa8ac2ceab345921
Atharv Joshi <atharv.joshi@contentstack.com>
2023-04-02 09:53:16 +0530

    created high level components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Banner from '@/components/Banner';
+import LandingPage from '@/components/LandingPage';
+
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const Home = (props : Object) => {
   return (
     <>
+    <Header></Header>
+    <Banner></Banner>
+    <LandingPage></LandingPage>
+    <Footer></Footer>
     </>
   )
 }
+
+export function getStaticProps(){
+  return {
+    "props" : {}
+  };
+}
+
+export default Home
